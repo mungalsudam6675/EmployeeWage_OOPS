95399bfd6f45550e9698a897e707d6af57d2d881
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-02-04 12:56:43 +0530

    Claculate employee wage for a month

--- Start.java
+++ Start.java
@@ -7,29 +7,38 @@ public class Start {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int EMP_WAGE_PER_HRS = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
 
 	public static void main(String[] args) {
-		int check=Utility.empCheck();
-		switch(check) {
-			case IS_FULL_TIME : {
-				int empHrs=8;
-				int empWage = empHrs * EMP_WAGE_PER_HRS; 
-				System.out.println("Employee Wage = " + empWage);
-				break;
-			}
+		int empHrs = 0;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		
+		for(int day=1; day<=NUM_OF_WORKING_DAYS; day++) {
+			int check=Utility.empCheck();
+			switch(check) {
+				case IS_FULL_TIME : {
+					empHrs = 8;
+					break;
+				}
 			
-			case  IS_PART_TIME :{
-				int empHrs=4;
-				int empWage = empHrs * EMP_WAGE_PER_HRS; 
-				System.out.println("Employee Wage = " + empWage);
-				break;
-			}
+				case  IS_PART_TIME :{
+					empHrs = 4;
+					break;
+				}
 			
-			default : {
-				System.out.println("Employee is Absent");
+				default : {
+					 empHrs = 0;
+				}
+			
 			}
+			empWage = empHrs * EMP_WAGE_PER_HRS;
+
+			totalEmpWage += empWage;
 		
 		}
+		
+		System.out.println("Total month Employee wage is " +totalEmpWage);
 		
 	}
 
