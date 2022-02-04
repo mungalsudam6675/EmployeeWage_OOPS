61692ceaf39242e8ed05567ff1835c3fa088887e
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-02-04 12:12:11 +0530

    Adding usecase 2 to calculate employee wage

--- Start.java
+++ Start.java
@@ -3,11 +3,16 @@ package EmpWage_OOPS;
 import Com.OOPS.Utility.Utility;
 
 public class Start {
+	
+	public static final int IS_FULL_TIME = 1;
+	public static final int EMP_WAGE_PER_HRS = 20;
 
 	public static void main(String[] args) {
-		int check=Utility.empCheck();
-		if(check == 1) {
-			System.out.println("Employee is Present");
+		int check=Utility.empCheck();	
+		if(check == IS_FULL_TIME) {
+			int empHrs=8;
+			int empWage = empHrs * EMP_WAGE_PER_HRS; 
+			System.out.println("Employee Wage = " + empWage);
 		}
 		else {
 			System.out.println("Employee is Absent");
