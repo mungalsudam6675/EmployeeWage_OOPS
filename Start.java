ae68032dcfb186339a111c62b3c51c15b8033fc6
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-02-05 12:41:03 +0530

    Calculate employee wage for multiple company

--- Start.java
+++ Start.java
@@ -7,18 +7,15 @@ public class Start {
 	
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
-	public static final int EMP_WAGE_PER_HRS = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_NUM_OF_HRS = 100;
 	
-	public static int calculateWage() {
+	public static int calculateEmpWage(String company,int empWagePerHr, int numOfWorkingDays,int maxNumOfHrs) {
 		
 		int empHrs = 0;
 		int totalEmpWage = 0;
 		int totalWorkingDays = 0;
 		int totalEmpHrs = 0;
 		
-		while( totalEmpHrs < MAX_NUM_OF_HRS && totalWorkingDays < NUM_OF_WORKING_DAYS ) {
+		while( totalEmpHrs < maxNumOfHrs && totalWorkingDays < numOfWorkingDays ) {
 			totalWorkingDays++;
 			int check=Utility.empCheck();
 			switch(check) {
@@ -43,7 +40,7 @@ public class Start {
 		
 		}
 		
-		totalEmpWage = totalEmpHrs * EMP_WAGE_PER_HRS;
+		totalEmpWage = totalEmpHrs * empWagePerHr;
 		
 		System.out.println("Total month Employee wage is " +totalEmpWage);
 		return totalEmpWage;	
@@ -52,7 +49,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		
-		calculateWage();
+		calculateEmpWage("Dmart",20,2,10);
+		calculateEmpWage("Reliance",20,2,10);
 		
 		
 	}
